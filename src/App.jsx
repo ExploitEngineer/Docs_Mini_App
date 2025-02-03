5621f30a8da315a8708154d38baf999ffa05115d
ExploitEngineer <revilwhoami0098@gmail.com>
2025-02-04 00:23:04 +0500

    Add Initial Commit of Docs_Mini_App

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return(
+        <>
+            <div className="bg-zinc-800 w-full min-h-screen"></div>
+        </>
+    )
+}
+
+export default App;
